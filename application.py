4e5a3091986e5d8640dde1610a5a40bc4ec7b21a
Mengyang Liu <drmyang@amazon.com>
2023-12-18 23:31:10 +0000

    Move common logic to a handle function.

--- application.py
+++ application.py
@@ -1,4 +1,3 @@
-import json
 import sys
 import os 
 import flask 
@@ -14,58 +13,36 @@ def hello_world():
         'flask_version': flask.__version__,
     })
 
+def handle(command: str):
+    command_content = 'file not exist'
+    if os.path.exists(f"{command}.txt"):
+        with open(f"{command}.txt", "r") as f:
+            command_content = f.read()
+    return flask.jsonify({
+        'name': 'flaskapp',
+        'python_version': sys.version.split(" ", 2)[0],
+        'environment': os.environ.copy(),
+        'flask_version': flask.__version__,
+        f'{command}': command_content
+    })
+    
+
 @application.route('/pre-build')
 def pre_build():
-    with open("pre-build.txt", "r") as f:
-        return flask.jsonify({
-            'name': 'flaskapp',
-            'python_version': sys.version.split(" ", 2)[0],
-            'environment': os.environ.copy(),
-            'flask_version': flask.__version__,
-            'pre-build': f.read()
-        })
+    return handle('pre-build')
 
 @application.route('/build')
 def build():
-    with open("build.txt", "r") as f:
-        return flask.jsonify({
-            'name': 'flaskapp',
-            'python_version': sys.version.split(" ", 2)[0],
-            'environment': os.environ.copy(),
-            'flask_version': flask.__version__,
-            'build': f.read()
-        })
+    return handle('build')
+
+@application.route('/post-build')
+def post_build():
+    return handle('post-build')
 
 @application.route('/pre-run')
 def pre_run():
-    with open("pre-run.txt", "r") as f:
-        return flask.jsonify({
-            'name': 'flaskapp',
-            'python_version': sys.version.split(" ", 2)[0],
-            'environment': os.environ.copy(),
-            'flask_version': flask.__version__,
-            'pre-run': f.read()
-        })
-
-@application.route('/post-build')
-def pre_run():
-    if os.path.exists("post-build.txt"):
-        with open("post-build.txt", "r") as f:
-            return flask.jsonify({
-                'name': 'flaskapp',
-                'python_version': sys.version.split(" ", 2)[0],
-                'environment': os.environ.copy(),
-                'flask_version': flask.__version__,
-                'post-build': f.read()
-            })
-    else:
-        return flask.jsonify({
-            'name': 'flaskapp',
-            'python_version': sys.version.split(" ", 2)[0],
-            'environment': os.environ.copy(),
-            'flask_version': flask.__version__,
-            'post-build': 'file not exist'
-        })
+    return handle('pre-run')
+    
 
 if __name__ == '__main__':
     port = int(os.environ.get("FLASK_RUN_PORT", 8000))
